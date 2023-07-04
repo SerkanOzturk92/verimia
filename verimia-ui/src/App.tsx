3e5813ff0004caf20e708167a7f39b0f672ae776
Serkan Öztürk <36485369+SerkanOzturk92@users.noreply.github.com>
2023-07-04 21:43:28 +0300

    update packages

--- verimia-ui/src/App.tsx
+++ verimia-ui/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const people = [
+        {
+            name: 'Calvin Hawkins',
+            email: 'calvin.hawkins@example.com',
+            image:
+                'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+            name: 'Kristen Ramos',
+            email: 'kristen.ramos@example.com',
+            image:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+            name: 'Ted Fox',
+            email: 'ted.fox@example.com',
+            image:
+                'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    ]
+
+    return (
+        <div className="App">
+            Hello world
+        </div>
+    );
 }
 
 export default App;
